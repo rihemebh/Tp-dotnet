f82f45133e038837681cc7debd7bea042a8f0253
rihemebh <rihemebenhassan@insat.u-carthage.tn>
2021-05-31 12:07:04 +0100

    tp1

--- Tp1/AppBancaire/AppBancaire/Compte.cs
+++ Tp1/AppBancaire/AppBancaire/Compte.cs
@@ -6,8 +6,11 @@ namespace AppBancaire
 {
     class Compte
     {
-        public decimal Solde { get;  }
+        protected decimal  solde;
+        public virtual decimal Solde { get => solde; set => solde = value; }
         public string Prop { set; get; }
+        public  List<operationBancaire> operations;
+       
         public Compte() : this("",0)
         {
 
@@ -16,38 +19,101 @@ namespace AppBancaire
         {
             this.Solde = solde;
             this.Prop = prop;
+            operations = new List<operationBancaire>();
 
         }
-      
-        private void Crediter(decimal Somme)
+        public  void Addoperation(decimal montant, string type)
         {
+            operations.Add(new operationBancaire(montant, type));
+        }
+        public virtual void Crediter(decimal Somme)
+        {
+            Addoperation(Somme, "Crediter");
+            Solde +=Somme;
 
         }
-        private void Crediter(decimal Somme,  Compte Cpt)
+        public virtual void Crediter(decimal Somme, Compte Cpt)
         {
-
+         
+            Cpt.Crediter(Somme);
         }
-        private void Debiter(decimal Somme)
-        {
 
-        }
-        private void Debiter(decimal Somme, Compte Cpt)
+        public virtual void Debiter(decimal Somme)
         {
-
+            Addoperation(Somme, "Debiter");
+            Solde -=Somme;
         }
-         public void Afficher()
+        public virtual void Debiter(decimal Somme, Compte Cpt)
         {
-            Console.WriteLine("le compte de " + this.Prop + "contient une somme de " + this.Solde.ToString() + " Dt");
+            Cpt.Debiter(Somme);
+        }
+         public virtual void Afficher()
+        {
+            Console.WriteLine("le compte de " + this.Prop + " \n contient une somme de " + this.Solde.ToString() + " Dt \n");
+            foreach (var op in operations)
+            {
+                Console.WriteLine(op.type + " : " + op.montant);
+            }
         } 
 
     }
-
-    class CompteEpargne : Compte
+    class operationBancaire
     {
 
+        public decimal montant { get; set; }
+        public string type;
+
+        public operationBancaire(decimal montant, string type)
+        {
+            this.montant = montant;
+            this.type = type;
+        }
+        public operationBancaire()
+        {
+           
+        }
+       
     }
     class CompteCourant : Compte
     {
-        public 
+        private readonly double decouvert;
+
+      
+    public CompteCourant(double decouvert) : base()
+        {
+            this.decouvert = decouvert;
+        }
+        public CompteCourant(double decouvert, string prop, decimal solde) : base(prop, solde)
+        {
+            this.decouvert = decouvert;
+        }
+
+        public override void Afficher()
+        {
+            base.Afficher();
+            Console.WriteLine("decouvert : " + decouvert);
+
+
+        }
+
+    }
+    class CompteEpargne : Compte
+    {
+        public decimal TauxAbond{ get; set; }
+
+     public  override decimal Solde { get { return base.solde + base.solde * TauxAbond; } set { base.solde = value; } }
+
+    public CompteEpargne(decimal solde, string proprietaire, decimal tauxAbond) : base(proprietaire, solde)
+    {
+        TauxAbond = tauxAbond;
+    }
+    public override void Afficher()
+        {
+            base.Afficher();
+            Console.WriteLine("taux  : " + TauxAbond);
+
+
+        }
+
     }
 }
--- Tp1/AppBancaire/AppBancaire/Program.cs
+++ Tp1/AppBancaire/AppBancaire/Program.cs
@@ -6,7 +6,46 @@ namespace AppBancaire
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+
+            CompteCourant cr = new CompteCourant(0.0, "david", 1000);
+      
+            CompteCourant cr1= new CompteCourant(0.0, "omar", 20000);
+          
+            CompteEpargne ce = new CompteEpargne( 5000, "salim",(decimal) 0.12);
+        
+            CompteEpargne ce1 = new CompteEpargne(5000, "Mourad", (decimal)0.02);
+        
+
+            cr.Crediter(50);
+            cr.Crediter(150);
+            cr.Debiter(70);
+            Console.WriteLine("******************************************");
+            cr.Afficher();
+
+
+            cr1.Crediter(150);
+            cr1.Crediter(20);
+            cr1.Debiter(700);
+            Console.WriteLine("******************************************");
+            cr1.Afficher();
+
+
+
+            ce.Crediter(60);
+            ce.Crediter(80);
+            ce.Debiter(25);
+            Console.WriteLine("******************************************");
+            ce.Afficher();
+
+
+            ce1.Crediter(5);
+            ce1.Crediter(58);
+            ce1.Debiter(87);
+            Console.WriteLine("******************************************");
+            ce1.Afficher();
+
+
         }
     }
 }
